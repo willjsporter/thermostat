18767882391f5cc23731cbcd8891fa225b95303d
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-31 15:54:16 +0100

    set powersafe mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -27,9 +27,16 @@ describe("Thermostat", function() {
     }).toThrow("Minimum temp is 10C");
   });
 
-  it("should throw an error when temp goes above 25C", function(){
+  it("should throw an error when temp goes above 25C by default", function(){
     expect(function() {
       thermo.change(11);
     }).toThrow("Maximum temp is 25C on powersafe mode");
+  });
+
+  it("should throw an error when temp goes above 32C when powersafe is off", function(){
+    expect(function() {
+      thermo.powersafe = false
+      thermo.change(21);
+    }).toThrow("Maximum temp is 32C when powersafe is off");
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,12 +2,16 @@
 
 var Thermostat = function (){
   this.temp=20;
+  this.powersafe = true
   this.change = function(num){
     if(this.temp+num<10){
       throw "Minimum temp is 10C"
     }
-    else if(this.temp+num>25){
+    else if(this.temp+num>25 && this.powersafe){
       throw "Maximum temp is 25C on powersafe mode"
+    }
+    else if(this.temp+num>32){
+      throw "Maximum temp is 32C when powersafe is off"
     }
     else{this.temp=this.temp+num};
 
